c0afec74e148c029a778a20a8988e46df34606f1
Thomas Start <thomasstart@hotmail.com>
2019-04-13 16:42:13 +0200

    cross validation scores

--- Task3/datasetmsemae.py
+++ Task3/datasetmsemae.py
@@ -5,8 +5,6 @@ regression methods, and discuss the differences you find.
  and another describing its size, attributes, etc.)
 '''
 
-# https://opendata.cbs.nl/statline/#/CBS/en/dataset/37259eng/table?dl=C09F
-# dataset with population growth of Amsterdam
 import csv
 import pandas as pd
 import numpy as np
@@ -15,38 +13,17 @@ import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
 from sklearn.metrics import mean_squared_error
 from sklearn.metrics import mean_absolute_error
+from sklearn.datasets import load_boston
+from sklearn.model_selection import cross_val_score
+from sklearn.tree import DecisionTreeRegressor
 
+boston = load_boston()
+print(boston.data.shape)
 
-# info = pd.read_csv('pdams.csv', sep=';', error_bad_lines=False)
-info = pd.read_csv('pdams2.csv', sep=';')
+regressor = DecisionTreeRegressor(random_state=0, criterion='mse')
+cvs = cross_val_score(regressor, boston.data, boston.target, cv=5)
+print(cvs)
 
-
-# print(info.columns.values)
-# print(info['Periods'], info['Population on 1 January (number)'])
-
-periods = info['Periods'][7:14]
-population_ams = info['Population on 1 January (number)'][7:14]
-
-def func(x, a, b, c):
-	return a*x**3+b*x+c
-
-x = np.arange(0, len(periods), 1)
-popt, pcov = curve_fit(func, x, population_ams)
-
-y = []
-for i in x:
-	y.append(func(i, popt[0], popt[1], popt[2]))
-
-# plt.plot(periods, y)
-# plt.scatter(periods, population_ams)
-# plt.show()
-
-y_true = population_ams
-y_pred = y
-
-mse = mean_squared_error(y_true, y_pred, multioutput='raw_values')
-mae = mean_absolute_error(y_true, y_pred, multioutput='raw_values')
-
-
-print(mse)
-print(mae)
+regressor = DecisionTreeRegressor(random_state=0, criterion='mae')
+cvs = cross_val_score(regressor, boston.data, boston.target, cv=)
+print(cvs)
