9afdf8711d55d55d1753b55b8ce2d223d6d89549
GGillis <46448029+GGillis@users.noreply.github.com>
2019-05-13 18:01:46 +0200

    Update model_training.py

--- model_training.py
+++ model_training.py
@@ -1,4 +1,4 @@
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor
 from sklearn.ensemble import GradientBoostingRegressor
 from sklearn.datasets import make_friedman1
 from sklearn.metrics import mean_squared_error
@@ -16,44 +16,68 @@ import sklearn.model_selection as ms
 data = pd.read_csv('data/training_set_VU_DM_clean.csv', sep=';', nrows=10000)
 columns = list(data.columns)
 train, test = ms.train_test_split(data, random_state=42, test_size=0.3)
-yLab = list(['click_bool', 'booking_bool', 'gross_bookings_usd'])
-trainY = train['click_bool'] + 5 * train['booking_bool']
+yLab = list(['click_bool', 'booking_bool','score','position'])
+trainY = train['score']
 trainYM = train[list(['click_bool', 'booking_bool'])]
 trainX = train.drop(yLab, axis=1)
-testY = test['click_bool'] + 5 * test['booking_bool']
+testY = test['score']
 testYM = test[list(['click_bool', 'booking_bool'])]
 
 testX = test.drop(yLab, axis=1)
 
 np.random.seed(424242)
 
-varsUsed = ['visitor_hist_starrating',
-            'visitor_hist_adr_usd',
-            'prop_starrating',
-            'prop_review_score',
-            'prop_brand_bool',
-            'prop_location_score1',
-            'prop_location_score2',
-            'prop_log_historical_price',
-            'price_usd',
-            'promotion_flag',
-            'srch_length_of_stay',
-            'srch_booking_window',
-            'srch_adults_count',
-            'srch_children_count',
-            'srch_room_count',
-            'srch_saturday_night_bool',
-            'srch_query_affinity_score',
-            'visitor_hist_starrating_is_null',
-            'day',
-            'hour',
-            'srch_person_per_room',
-            'Friday',
+bools = [k for k in list(trainX.columns) if 'bool' in k]
+isNulls = [k for k in list(trainX.columns) if 'is_null' in k]
+weekDays = ['Friday',
             'Monday',
             'Saturday',
             'Sunday',
             'Thursday',
             'Tuesday']
+comps = ['unavailable_comp',
+         'available_comp',
+         'avg_price_comp']
+otherSelection = ['has_historical_price',
+                 # 'travel_distances',
+                  'delta_starrating',
+                  'srch_query_affinity_score',
+                  'price_usd',
+                  'promotion_flag',
+                  'prop_location_score1',
+                  'prop_location_score2',
+                  'srch_adults_per_room_score',
+                  'srch_person_per_room_score']
+dateTimes = ['day',
+             'hour']
+varsUsed = bools + isNulls + weekDays + comps + otherSelection
+#varsUsed = ['visitor_hist_starrating',
+#            'visitor_hist_adr_usd',
+#            'prop_starrating',
+#            'prop_review_score',
+#            'prop_brand_bool',
+#            'prop_location_score1',
+#            'prop_location_score2',
+#            'prop_log_historical_price',
+#            'price_usd',
+#            'promotion_flag',
+#            'srch_length_of_stay',
+#            'srch_booking_window',
+#            'srch_adults_count',
+#            'srch_children_count',
+#            'srch_room_count',
+#            'srch_saturday_night_bool',
+#            'srch_query_affinity_score',
+#            'visitor_hist_starrating_is_null',
+#            'day',
+#            'hour',
+#            'srch_person_per_room',
+#            'Friday',
+#            'Monday',
+#            'Saturday',
+#            'Sunday',
+#            'Thursday',
+#            'Tuesday']
 
 # start training models below
 
@@ -65,5 +89,10 @@ est.feature_importances_
 
 rfest = RandomForestClassifier(n_estimators=100, max_depth=2, random_state=0)
 rfest.fit(trainX[varsUsed], trainYM)
-rfest.feature_importances_
 mean_squared_error(testYM, rfest.predict(testX[varsUsed]))
+a2 = rfest.predict(testX[varsUsed])
+
+rfReg = RandomForestRegressor(n_estimators=100)
+rfReg.fit(trainX[varsUsed], trainY)
+a3 = rfReg.predict(testX[varsUsed])
+#predMat = pd.DataFrame()
